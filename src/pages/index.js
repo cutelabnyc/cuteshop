ca8996aa58ca11ebe33bdb2e2408be65dd29a41b
Max Ardito <maxwellardito@gmail.com>
2021-01-08 21:17:01 -0500

    Home page points to recent product

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,101 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import SEO from '~/components/seo'
-import ProductGrid from '~/components/ProductGrid'
+import ProductForm from '~/components/ProductForm'
+import {
+    Img,
+    Container,
+    TwoColumnGrid,
+    GridLeft,
+    GridRight,
+} from '~/utils/styles'
+import { ProductTitle, ProductDescription } from '../templates/ProductPage/styles'
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" keywords={[`CuteLab`, `Modular`, `Eurorack`]} />
-    <ProductGrid />
-  </>
-)
+const IndexPage = ({ data }) => {
+    const query = useStaticQuery(
+        graphql`
+            query {
+                allShopifyProduct(sort: { fields: [id], order: DESC }) {
+                    edges {
+                        node {
+                                id
+                                title
+                                handle
+                                productType
+                                description
+                                descriptionHtml
+                                shopifyId
+                                options {
+                                    id
+                                    name
+                                    values
+                                }
+                                variants {
+                                    id
+                                    title
+                                    price
+                                    availableForSale
+                                    shopifyId
+                                    selectedOptions {
+                                    name
+                                    value
+                                    }
+                                }
+                                priceRange {
+                                    minVariantPrice {
+                                    amount
+                                    currencyCode
+                                    }
+                                    maxVariantPrice {
+                                    amount
+                                    currencyCode
+                                    }
+                                }
+                                images {
+                                    originalSrc
+                                    id
+                                    localFile {
+                                    childImageSharp {
+                                        fluid(maxWidth: 910) {
+                                        ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                                        }
+                                    }
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        `
+    )
+
+    const product = data.allShopifyProduct.edges[0].node
+    return (
+        <>
+            <SEO title={product.title} description={product.description} />
+            <Container>
+                <TwoColumnGrid>
+                    <GridLeft>
+                        {product.images.map(image => (
+                            <Img
+                                fluid={image.localFile.childImageSharp.fluid}
+                                key={image.id}
+                                alt={product.title}
+                            />
+                        ))}
+                    </GridLeft>
+                    <GridRight>
+                        <ProductTitle>{product.title}</ProductTitle>
+                        <ProductDescription
+                            dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+                        />
+                        <ProductForm product={product} />
+                    </GridRight>
+                </TwoColumnGrid>
+            </Container>
+        </>
+    )
+}
 
 export default IndexPage
