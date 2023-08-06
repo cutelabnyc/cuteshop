2fa5118b1166f6857cce4dc52513d298a7adbaa0
maxardito <maxwellardito@gmail.com>
2023-08-06 19:34:52 -0400

    Fixed product page URL paths

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -21,29 +21,29 @@ import {
 } from './styles'
 import ProductImages from '../../components/assets/product-assets'
 import { Gallery } from 'react-grid-gallery'
-import Lightbox from "react-image-lightbox";
-import "react-image-lightbox/style.css";
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
 import { MenuLink } from '../../components/Navigation/styles'
 
 const ProductPage = ({ data }) => {
     const product = data.shopifyProduct
     const collection = data.shopifyCollection
-    const isMessedUp = product.title.toLowerCase().search("messed up") >= 0;
-    const med = product.media;
-    const images = isMessedUp ? ProductImages.messed_up : ProductImages.missed_ops;
+    const isMessedUp = product.title.toLowerCase().search('messed up') >= 0
+    const med = product.media
+    const images = isMessedUp ? ProductImages.messed_up : ProductImages.missed_ops
 
-    const [index, setIndex] = useState(-1);
+    const [index, setIndex] = useState(-1)
 
-    const currentImage = images[index];
-    const nextIndex = (index + 1) % images.length;
-    const nextImage = images[nextIndex] || currentImage;
-    const prevIndex = (index + images.length - 1) % images.length;
-    const prevImage = images[prevIndex] || currentImage;
-  
-    const handleClick = (index, item) => setIndex(index);
-    const handleClose = () => setIndex(-1);
-    const handleMovePrev = () => setIndex(prevIndex);
-    const handleMoveNext = () => setIndex(nextIndex);
+    const currentImage = images[index]
+    const nextIndex = (index + 1) % images.length
+    const nextImage = images[nextIndex] || currentImage
+    const prevIndex = (index + images.length - 1) % images.length
+    const prevImage = images[prevIndex] || currentImage
+
+    const handleClick = (index, item) => setIndex(index)
+    const handleClose = () => setIndex(-1)
+    const handleMovePrev = () => setIndex(prevIndex)
+    const handleMoveNext = () => setIndex(nextIndex)
 
     return (
         <>
@@ -53,7 +53,7 @@ const ProductPage = ({ data }) => {
                     {collection.products.map((product, key) => {
                         return (
                             <MenuLink
-                                to={`${collection.handle}/${product.handle}`}
+                                to={`/${collection.handle}/${product.handle}`}
                                 key={key}
                             >
                                 {product.title}
@@ -67,9 +67,7 @@ const ProductPage = ({ data }) => {
                     <TwoColumnGrid>
                         <GridLeft>
                             <Img
-                                image={
-                                    med[0].image.gatsbyImageData
-                                }
+                                image={med[0].image.gatsbyImageData}
                                 key={med[0].id}
                                 alt={product.title}
                             />
@@ -100,16 +98,16 @@ const ProductPage = ({ data }) => {
             {!!currentImage && (
                 /* @ts-ignore */
                 <Lightbox
-                mainSrc={currentImage.original}
-                imageTitle={currentImage.caption}
-                mainSrcThumbnail={currentImage.src}
-                nextSrc={nextImage.original}
-                nextSrcThumbnail={nextImage.src}
-                prevSrc={prevImage.original}
-                prevSrcThumbnail={prevImage.src}
-                onCloseRequest={handleClose}
-                onMovePrevRequest={handleMovePrev}
-                onMoveNextRequest={handleMoveNext}
+                    mainSrc={currentImage.original}
+                    imageTitle={currentImage.caption}
+                    mainSrcThumbnail={currentImage.src}
+                    nextSrc={nextImage.original}
+                    nextSrcThumbnail={nextImage.src}
+                    prevSrc={prevImage.original}
+                    prevSrcThumbnail={prevImage.src}
+                    onCloseRequest={handleClose}
+                    onMovePrevRequest={handleMovePrev}
+                    onMoveNextRequest={handleMoveNext}
                 />
             )}
         </>
@@ -117,7 +115,7 @@ const ProductPage = ({ data }) => {
 }
 
 export const query = graphql`
-    query($handle: String!, $collectionHandle: String!) {
+    query ($handle: String!, $collectionHandle: String!) {
         shopifyProduct(handle: { eq: $handle }) {
             id
             title
@@ -154,12 +152,12 @@ export const query = graphql`
             }
             media {
                 ... on ShopifyMediaImage {
-                  id
-                  image {
-                    gatsbyImageData
-                  }
+                    id
+                    image {
+                        gatsbyImageData
+                    }
                 }
-              }
+            }
         }
         shopifyCollection(handle: { eq: $collectionHandle }) {
             handle
