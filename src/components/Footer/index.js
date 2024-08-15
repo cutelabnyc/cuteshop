1a3f560174377d94be55f6f18f1cff8cc14ac8be
maxardito <maxwellardito@gmail.com>
2024-08-15 22:37:34 +0200

    Fixed broken footer links and gowanus png

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,52 +1,52 @@
 import React from 'react'
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby'
 
 import {
-    Container,
-    MenuLink,
-    CuteLabLink,
-    FooterContainer,
-    Copyright
+  Container,
+  MenuLink,
+  CuteLabLink,
+  FooterContainer,
+  Copyright,
 } from './styles'
 
 const Footer = () => {
-    const { allMarkdownRemark } = useStaticQuery(
-        graphql`
-                query {
-                    allMarkdownRemark {
-                        edges {
-                            node {
-                                frontmatter {
-                                    title
-                                    slug
-                                }
-                            }
-                        }
-                    }
-                }
-            `
-    )
-    return (
-        <FooterContainer>
-            <Container>
-                {
-                    allMarkdownRemark.edges.map((page, key) => {
-                        return (
-                            <MenuLink to={page.node.frontmatter.slug} key={key}>
-                                {page.node.frontmatter.title}
-                            </MenuLink>
-                        )
-                    })
-                }
-            </Container>
-            <Container>
-                <CuteLabLink>
-                    <a href="http://www.cutelab.nyc">CuteLab NYC</a>
-                </CuteLabLink>
-                <Copyright>© {new Date().getFullYear()}</Copyright>
-            </Container>
-        </FooterContainer>
-    )
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  return (
+    <FooterContainer>
+      <Container>
+        {allMarkdownRemark.edges.map((page, key) => {
+          return (
+            <MenuLink to={`/${page.node.frontmatter.slug}`} key={key}>
+              {page.node.frontmatter.title}
+            </MenuLink>
+          )
+        })}
+      </Container>
+      <Container>
+        <CuteLabLink>
+          <a href="http://www.cutelab.nyc" target="_blank">
+            CuteLab NYC
+          </a>
+        </CuteLabLink>
+        <Copyright>© {new Date().getFullYear()}</Copyright>
+      </Container>
+    </FooterContainer>
+  )
 }
 
 export default Footer
